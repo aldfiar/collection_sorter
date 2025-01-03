acbf2509bef25cec10153245c82e079d20e4949f
aldfiar <darthmantis@yandex.ru>
2025-01-03 22:56:31 +0300

    fix

--- collection_sorter/common/move.py
+++ collection_sorter/common/move.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 from typing import Callable
 import shutil
-from collection_sorter.common.path import CollectionPath
+from .files import CollectionPath
 
 logger = logging.getLogger('move')
 
@@ -11,7 +11,7 @@ class MovableCollection(CollectionPath):
 
     def _change(self, new_path: Path, command: Callable[[Path, Path], None]) -> CollectionPath:
         if not new_path.exists():
-            new_path.mkdir(parents=True, mode=0o755)
+            new_path.mkdir(parents=True, mode=0755)
 
         for file in self.get_files():
             source_path = file.resolve()
@@ -23,8 +23,7 @@ class MovableCollection(CollectionPath):
         return CollectionPath(new_path)
 
     def copy(self, new_path: Path) -> CollectionPath:
-        shutil.copy2 = shutil.copy
-        return self._change(new_path, shutil.copy2)
+        return self._change(new_path, shutil.copy)
 
     def move(self, new_path: Path) -> CollectionPath:
         return self._change(new_path, shutil.move)
--- tests/test_move.py
+++ tests/test_move.py
@@ -9,63 +9,43 @@ from collection_sorter.common.move import MovableCollection
 def reverse(st):
     return '0' if st == '1' else '0'
 
-def flipTwo(some, i):
-    some[i] = reverse(some[i])
-    next_symbol = i + 1
-    some[next_symbol] = reverse((some[next_symbol]))
-
-def getMinimumOperations(binaryStr):
-    maxConverts = 0
-    for i in range(0, len(binaryStr))
 
 class TestMover(TestCase):
 
-    # def setUp(self) -> None:
-    #     self.source_temp = tempfile.TemporaryDirectory()
-    #     self.source = Path(self.source_temp.name)
-    #     self.destination_temp = tempfile.TemporaryDirectory()
-    #     self.destination = Path(self.destination_temp.name)
-    #     self.files = []
+    def setUp(self) -> None:
+        self.source_temp = tempfile.TemporaryDirectory()
+        self.source = Path(self.source_temp.name)
+        self.destination_temp = tempfile.TemporaryDirectory()
+        self.destination = Path(self.destination_temp.name)
+        self.files = []
 
 
 
-    def test_flip(self):
-        string = "1011"
-        f
 
-    def test_some(self):
-        b = [[2, 3], [5, 7]]
-        after = [[2, 5], [7, 17]]
 
-        for i in range(len(after) - 1, 0, -1):
-            temp = after[i]
-            for j in range(0, len(after[i])):
-                print(after[i][j])
+    def test_move_file_to_dest(self):
+        stf = tempfile.TemporaryFile(dir=str(self.source.absolute()))
+        self.files.append(stf)
+        ftm = Path(stf.name)
+        collection = MovableCollection(self.source)
+        collection.move(self.destination)
+        expected = self.destination.joinpath(ftm.name)
+        self.assertTrue(expected.exists())
 
-    #
-    # def test_move_file_to_dest(self):
-    #     stf = tempfile.TemporaryFile(dir=str(self.source.absolute()))
-    #     self.files.append(stf)
-    #     ftm = Path(stf.name)
-    #     collection = MovableCollection(self.source)
-    #     collection.move(self.destination)
-    #     expected = self.destination.joinpath(ftm.name)
-    #     self.assertTrue(expected.exists())
-    #
-    #
-    # def test_copy_file_to_dest(self):
-    #     stf = tempfile.TemporaryFile(dir=str(self.source.absolute()))
-    #     ftm = Path(stf.name)
-    #     collection = MovableCollection(self.source)
-    #     collection.copy(self.destination)
-    #     expected = self.destination.joinpath(ftm.name)
-    #     self.assertTrue(expected.exists())
-    #     self.assertTrue(ftm.exists())
-    #
-    # def tearDown(self) -> None:
-    #     self.source_temp.cleanup()
-    #     self.destination_temp.cleanup()
-    #     for file in self.files:
-    #         file.close()
-    #
-    #
+
+    def test_copy_file_to_dest(self):
+        stf = tempfile.TemporaryFile(dir=str(self.source.absolute()))
+        ftm = Path(stf.name)
+        collection = MovableCollection(self.source)
+        collection.copy(self.destination)
+        expected = self.destination.joinpath(ftm.name)
+        self.assertTrue(expected.exists())
+        self.assertTrue(ftm.exists())
+
+    def tearDown(self) -> None:
+        self.source_temp.cleanup()
+        self.destination_temp.cleanup()
+        for file in self.files:
+            file.close()
+
+

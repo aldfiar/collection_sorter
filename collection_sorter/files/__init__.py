ee4adb84774febdd0552823f32c90c43422f4d91
Ivan Sheckoldin <darthmantis@yandex.ru>
2025-05-08 10:33:06 +0300

    fix imports

--- collection_sorter/files/__init__.py
+++ collection_sorter/files/__init__.py
@@ -1,3 +1,2 @@
-from .move import MovableCollection
 from .files import CollectionPath
-from .paths import FilePath, DirectoryPath, PathType, FileLike, FileOperationError
+from .move import MovableCollection
